9eebe0982f9d115de8903098f6438ca7dc61e5fd
Idifavour <idifavour7@gmail.com>
2022-03-07 01:51:36 +0400

    second

---
+++ js/lessons.js
@@ -0,0 +1,47 @@
+let lessons = [
+    {
+        "id": 1001,
+        "subject": 'English',
+        "location": 'London',
+        "price": 5000,
+        "image": "/images/product.png",
+        "stock": 5
+
+    },
+    {
+        "id": 1002,
+        "subject": 'Music',
+        "location": 'Oxford',
+        "price": 4000,
+        "image": "/images/product.png",
+        "stock": 9
+
+    },
+    {
+        "id": 1003,
+        "subject": 'Maths',
+        "location": 'York',
+        "price": 7000,
+        "image": "/images/product.png",
+        "stock": 11
+
+    },
+    {
+        "id": 1004,
+        "subject": 'English',
+        "location": 'Bristol',
+        "price": 2000,
+        "image": "/images/product.png",
+        "stock": 8
+
+    },
+    {
+        "id": 1005,
+        "subject": 'Maths',
+        "location": 'London',
+        "price": 5000,
+        "image": "/images/product.png",
+        "stock": 10
+
+    },
+]
